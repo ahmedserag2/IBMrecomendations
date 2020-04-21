a6a4085750ab38fbcdd5465bde4e4e4f733ff67d
ahmed serag <ahmedserag2000@gmail.com>
2020-04-21 19:41:03 +0200

    started a basic flask run.py and note: template extension  not yet configured

--- program/app/run.py
+++ program/app/run.py
@@ -5,3 +5,24 @@ import pandas as pd
 
 from flask import Flask
 from flask import render_template, request, jsonify
+
+
+app = Flask(__name__)
+
+# index webpage displays cool visuals and receives user input text for model
+@app.route('/')
+@app.route('/index')
+def index():        
+    
+    return render_template('index.html')
+
+
+
+
+
+def main():
+    app.run(host='0.0.0.0', port=3001, debug=True)
+
+
+if __name__ == '__main__':
+    main()
